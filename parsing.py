1cb641a1ebe485f5eb430a50b4a1bbf42c7014cc
maksim <4403604@gmail.com>
2018-03-26 22:33:44 +0300

    Simple code for parsing a book

---
+++ parsing.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+base_url = 'http://book-online.com.ua/read.php?book=1&page='
+for i in range(1, 230):
+    page = requests.get(base_url + str(i))
+    soup = BeautifulSoup(page.text, 'html.parser')
+    t = soup.find('div', id='ptext').find_all('p')
+    count = len(t)
+    filename = 'book.txt'
+    for i in range(1, count):
+        fix = t[i].text
+        filename = open('book.txt','a')
+        filename.write(t[i].text + '\n')
+        filename.close()
+        print(fix)
