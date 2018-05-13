5ad9c40b510a5a2fd531daf017e71d8cd8c71ae8
maksim <4403604@gmail.com>
2018-05-13 22:19:02 +0300

    changed function description

--- new_parsing.py
+++ new_parsing.py
@@ -1,41 +1,41 @@
-'''Импортируем модуль Requests, чтобы собрать данные с веб-страницы.
-Библиотека Beautiful Soup создает дерево синтаксического разбора и сделает текст веб-страницы,
-извлеченный с помощью Requests, более удобочитаемым.'''
+"""Import the Requests module to collect data from a web page. The Beautiful Soup library creates
+ a parsing tree and makes the text of the web page retrieved using Requests more readable."""
 import requests
 from bs4 import BeautifulSoup
 
-'''Функция принимает на входе url обрабатывается модулем request и на выходе
-возвращает  текстовое содержимае веб страницы'''
 
 def get_html(url):
+    """The function accepts the url at the input is processed by request module and returns
+    the text content of the web page at the output"""
     page = requests.get(url)
     return page.text
 
-'''Функция определения количества страниц книги. Принимает на входе текстовое содержимое веб страницы,
- затем получает обьект Soup через встроеный парсер.Находим id секцию содержащую теги с ссылками и получаем последнюю ссылку.
- Сплитим сылку и забираем число  которое было после =. Возвращаем число переводя его из str в int'''
 
 def get_total_pages(get_html):
+    """Function of determining the number of pages of the book.
+    Accepts the text content of the web page at the input, then receives the Soup object through the built-in parser.
+    Find the section id containing the tags of the links and get latest link.
+    Do split exile and take away the number which was after =. Return the number by converting it from str to int"""
     soup = BeautifulSoup(get_html, 'lxml')
     pages = soup.find('div', id="div_paginator").find_all('a')[-1].get('href')
     total_pages = pages.split('=')[2]
     return int(total_pages)
 
-''' Функция получения низвания книги.Передаем текстовое содержимое страницы затем получаем обьект soup.
-В нем исчем секцию id с текстом и в нем есчем и забираем название.Фарматируем  строку с названием добавляя расширени .txt.
-  Возвращем название книги в формате "Name.txt".'''
 
 def get_name(get_html):
+    """Function to obtain the title of the book.Pass the text content of the page then get the soup object.
+    In it we look for the id section with the text and in it we look for and take away the name.
+    Formatted string with title adding extended .txt. Return the book name in the format " Name.txt"."""
     soup = BeautifulSoup(get_html, 'lxml')
     book = soup.find('div', id='ptext').find_all('p')[1].text
     name_book = "{}.txt".format(book)
     return name_book
 
-'''Основная функция. Получает через ввод url, содержит макет построения ссылки,
- через цикл for  генерирует url исходя из общего количества страниц, получает её обробатывает парсером,
-  делает поиски по секциям id, для поиска текста.Затем создает фаил и по строчно записывает в него текст.'''
 
 def main():
+    """Main function. Receive, through the input url contains a layout of building links, through the loop generates
+    a url based on the total number of pages, it gets processed by the parser, making the search for the sections id
+    to search for text.Then creates the file and writes the text line by line"""
     url = input('Enter the URL: ')
     base_url = url
     page_part = '&page='
